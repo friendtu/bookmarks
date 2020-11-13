66513f9d03dcc551fbcade384366b23e2f398760
friendtu <friendtu@gmail.com>
2020-11-13 19:51:31 +0800

    add login page

--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.contrib.auth import authenticate,login
+from .forms import LoginForm
+from django.http import HttpResponse
 # Create your views here.
+
+def user_login(request):
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponse('Authenticated successfully')
+            else:
+                return HttpResponse('Invalid account')
+    else:
+        form=LoginForm()
+
+    return render(request,'account/login.html',{
+            'form':form
+        })
+
+
+    
