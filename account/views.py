536f095959ea45e32892ae56b05561d85bd69a52
friendtu <friendtu@gmail.com>
2020-11-14 18:10:39 +0800

    fix issue to go to next after login

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate,login
 from .forms import LoginForm
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def user_login(request):
@@ -24,5 +25,8 @@ def user_login(request):
             'form':form
         })
 
+@login_required
+def dashboard(request):
+    return render(request,'account/dashboard.html')
 
     
