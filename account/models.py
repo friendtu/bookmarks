9c2938b9b0c0d47dd3a495032625b5b6bed70ec4
friendtu <friendtu@gmail.com>
2020-11-16 21:44:16 +0800

    add models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+
+
+class Profile(model.Model):
+    user=models.OneToOneField(setting.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    date_of_birth=models.DateField(null=True,blank=True)
+    photo=models.ImageField(upload_to='uploads/%Y/%m/%d/',blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
