ad7f50cc807155bd734ebf9bb627af204f16be1c
friendtu <friendtu@gmail.com>
2020-11-25 21:16:04 +0800

    add profile and email authentication

--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm,UserRegistrationFrom
+from .forms import LoginForm,UserRegistrationFrom,UserEditForm,ProfileEditForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+
 # Create your views here.
 
 def user_login(request):
@@ -37,12 +39,35 @@ def register(request):
             new_user=user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            #create empty profile for the new user
+            Profile.objects.create(user=new_user)
+
+
+            
             return render(request,'account/register_done.html',
                                    {'new_user':new_user})
     else:
         user_form=UserRegistrationFrom()
     return render(request,'account/register.html',
                             {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method=='POST':
+        user_form=UserEditForm(request.POST,instance=request.user)
+        profile_form=ProfileEditForm(request.POST,instance=request.user.profile)
+        if user_form.is_valid and profile_form.is_valid:
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form=UserEditForm(instance=request.user)
+        profile_form=ProfileEditForm(instance=request.user.profile)
+    return render(request,'account/edit.html',
+                    {
+                        'user_form':user_form,
+                        'profile_form':profile_form
+                    }
+                 )
 
 
     
